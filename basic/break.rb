6bca47bf67368d8ef5dc4806473755639b819be1
myhearisranch <myhearisranch@gmail.com>
2021-11-14 15:44:37 +0000

    [Finish]2.2Rubyに関する基礎知識

---
+++ basic/break.rb
@@ -0,0 +1,16 @@
+1.to_s
+nil.to_s
+10.to_s
+
+1.to_s; nil.to_s; 10.to_s(16)
+
+10.to_s(
+  16
+  )
+  10.to_s
+  16
+  
+  10.to_s\
+  16
+  
+  
---
+++ basic/localvariable.rb
@@ -0,0 +1,29 @@
+s='Hello'
+n=123*2
+
+special_price = 200
+specialprice = 200
+
+_special_price = 200
+
+special_price_2 = 300
+2_special_price = 300
+
+特別価格 = 200
+
+x='Hello'
+x=123
+x='Good-bye'
+x= 456
+
+a,b=1,2
+a
+b
+
+e,f = 100, 200, 300
+e
+f
+
+a=b=100
+a
+b
