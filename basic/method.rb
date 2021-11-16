bef65ef34fc4b5f7cf7cd348a3ee6521d51aa219
myhearisranch <myhearisranch@gmail.com>
2021-11-16 01:20:03 +0000

    [Finish]2.6 メソッドの定義

--- basic/method.rb
+++ basic/method.rb
@@ -1,4 +1,70 @@
-1.to_s()
-1.to_s
-10.to_s(16)
-10.to_s(16)
+def add (a,b)
+
+  a+b
+
+end
+
+puts add(1,2)
+
+def hello_world
+    'Hello, world!'
+end
+
+def helloworld
+  'Hello, world!'
+end
+
+def _hello_world
+  'Hello, World!'
+end
+
+def hello_world2
+
+  'Hello, world!'
+
+end
+
+def 2_hello_world
+  'Hello, world'
+end
+
+def アイルランド
+  'ハロー'
+end
+
+def add(a,b)
+  return a+b
+end
+
+add(1,2)
+
+def greeting(country)
+
+  if country == 'japan'
+    'こんにちは'
+  else
+    'hello'
+  end
+
+  greeting('japan')
+  greeting('us')
+  
+  def greeting(country)
+    
+    return 'countryを入力してください' if country.nill
+    
+    if country =='japan'
+      'こんにちは'
+    else
+      'hello'
+    end
+  end
+  
+  greeting(nil)
+  greeting('japan')
+  
+  def greeting
+    'こんにちは'
+  end
+  
+  
