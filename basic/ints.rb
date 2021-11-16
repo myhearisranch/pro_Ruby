53b42e2ce57b7b176c736d91af48849046963e2a
myhearisranch <myhearisranch@gmail.com>
2021-11-16 03:42:13 +0000

    [Finish]2.9 数値についてもっと詳しく

---
+++ basic/ints.rb
@@ -0,0 +1,27 @@
+puts 0b111111
+puts 0377
+puts 0xff
+
+puts (~0b1010).to_s(2)
+
+puts  2e-3
+
+puts 10.class
+puts 1.5.class
+
+
+r = 2/3r
+r
+puts r.class
+
+r='2/3'.to_r
+r
+puts r.class
+
+c=0.3-0.5i
+c
+puts c.class
+
+c='0.3-0.5i'
+c
+puts c.class
