a55fa29be72b3382a0b46a59626684a43dab7007
myhearisranch <myhearisranch@gmail.com>
2021-11-16 01:06:46 +0000

    [Finish]2.5 真偽値と条件分岐

---
+++ basic/conditon.rb
@@ -0,0 +1,44 @@
+country = 'italy'
+
+greeting =
+
+if country == 'japan'
+  'こんにちは'
+elsif country == 'us'
+
+  'Hello'
+elsif country == 'italy'
+
+  'ciao'
+else
+  '???'
+end
+
+puts greeting
+
+point = 7
+day = 1
+
+if day == 1
+  point *=5
+end
+
+puts point
+
+point = 7
+day = 1
+
+point *= 5 if day == 1
+puts point
+
+count = 'italy'
+
+greeting2=
+
+if country == 'japan' then 'こんにちは'
+elsif country == 'us' then 'Hello'
+elsif country == 'italy' then 'ciao'
+else '???'
+end
+
+puts greeting2
