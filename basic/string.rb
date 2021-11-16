7d7548a7a73fc7973946ceba6fe7bf5fe2c3c288
myhearisranch <myhearisranch@gmail.com>
2021-11-16 00:45:59 +0000

    [Finish] 2.4 数値

---
+++ basic/string.rb
@@ -0,0 +1,47 @@
+'これは文字列です'
+"これも文字列です"
+
+puts "こんにちは\nさようなら"
+
+puts 'こんにちは\nさようなら'
+
+name ="Alice"
+
+puts "Hello, #{name}!"
+
+i=10
+
+puts "#{i}は16進数にすると#{i.to_s(16)}です"
+
+name = 'Alice'
+
+puts 'Hello, #{name}!'
+name ='Alice'
+puts 'Hello,'+name+'!'
+
+puts "こんにちは\\nさようなら"
+
+name = 'Alice'
+puts "Hello, \#{name}!"
+
+puts 'He said, "Don\'t speak."'
+puts "He said, \"Don't speak.\""
+
+puts'ruby' == 'ruby'
+puts 'Ruby' == 'ruby'
+puts 'ruby' != 'perl'
+puts 'ruby' != 'ruby'
+
+n=1
+n.to_f
+puts n.to_f/2
+
+puts 0.1*3.0
+
+puts 0.1*3.0==0.3
+puts 0.1*3.0<=0.3
+
+puts 0.1r*3.0r
+puts 0.1r*3.0r ==0.3
+puts 0.1r*3r <= 0.3
+
